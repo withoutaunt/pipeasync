f2b001c22e5bacdc9adf0f2bb56ac81fab80e2a8
withoutaunt <cinnalb@gmail.com>
2022-06-08 21:53:25 -0300

    creacion servicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MyServiceService } from './services/my-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'pipeasyncAlbini';
+arr:any=[];
+  constructor(private miServicio:MyServiceService){
+
+  }
+  ngOnInit(): void {
+    this.arr=this.miServicio.retornar();
+  }
 }
